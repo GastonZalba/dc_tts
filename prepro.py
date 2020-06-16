e20b202997bbe64985a1adf8341f3cb7e912bcd1
Gastón Zalba <34213485+tatoz12@users.noreply.github.com>
2020-06-16 01:05:24 -0300

    Fix mags and mels folder in prepro

--- prepro.py
+++ prepro.py
@@ -7,6 +7,7 @@ https://www.github.com/kyubyong/dc_tts
 
 from __future__ import print_function
 
+from hyperparams import Hyperparams as hp
 from utils import load_spectrograms
 import os
 from data_load import load_data
@@ -18,8 +19,13 @@ fpaths, _, _ = load_data() # list
 
 for fpath in tqdm.tqdm(fpaths):
     fname, mel, mag = load_spectrograms(fpath)
-    if not os.path.exists("mels"): os.mkdir("mels")
-    if not os.path.exists("mags"): os.mkdir("mags")
 
-    np.save("mels/{}".format(fname.replace("wav", "npy")), mel)
-    np.save("mags/{}".format(fname.replace("wav", "npy")), mag)
+    folder = "voces_procesadas/{}/".format(hp.voice)
+    melsFolder = folder + "mels"
+    magsFolder = folder + "mags"
+
+    if not os.path.exists(melsFolder): os.mkdir(melsFolder)
+    if not os.path.exists(magsFolder): os.mkdir(magsFolder)
+
+    np.save(melsFolder+"/{}".format(fname.replace("wav", "npy")), mel)
+    np.save(magsFolder+"/{}".format(fname.replace("wav", "npy")), mag)
